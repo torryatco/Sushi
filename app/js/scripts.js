2cff06dba8328aeeb8fa0583337efc943909c4dc
torryatco <torryatco@gmail.com>
2025-02-18 18:58:05 -0500

    added google map

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,22 +1,9 @@
-// var mapClicker = document.querySelector(".map");
-// var body = document.querySelector("body")
-
-// document.addEventListener("click", show);
-
-// function show(e) {
-//   e.preventDefault();
-//   // body.classList.toggle("showme")
-//   console.log(e.target)
-// }
-
-
 var body = document.querySelector("body");
 document.addEventListener("click", handleClicks);
 
 function handleClicks(e) {
-  
-  if (e.target.matches(".map")) {
-    body.classList.add("showme");
+  if (e.target.matches(".map") || e.target.matches(".closer")) {
+    body.classList.toggle("showme");
     e.preventDefault();
   } else {
     body.classList.remove("showme");
